69141d284174ee5bdf0c393d0a2f6a2f776da96c
Aarif-shazZ <shazaarif0@gmail.com>
2024-03-30 12:51:21 +0530

    done

--- Index.js
+++ Index.js
@@ -11,34 +11,26 @@ const server = http.createServer(app); // Create HTTP server
 
 const io = socketIo(server, {
     cors: {
-        origin: "*",
+        origin: "http://localhost:3002/",
         methods: ["GET", "POST"],
         credentials: true,
     },
 });
 
-
 app.get("/hello", (req, res) => {
     res.send("hello world");
 });
-
-
-
-// Start the server
-// server.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
 
 io.on("connection", (socket) => {
     // Listen for "current-location" event from the client
     console.log("Connected Client");
 
+    socket.emit("check",{name:"Aarif"})
     socket.on("disconnect", () => {
         console.log(`Client disconnected. Socket ID: ${socket.id
             }`);
     });
 });
-
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
